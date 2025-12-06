eeee0f98ac0c0a0dc851f6f57a4618ec03fb4f84
adityam1825 <156218384+adityam1825@users.noreply.github.com>
2025-12-07 00:20:28 +0530

    Comment out file uploader encoding option
    Comment out the deprecation warning setting for file uploader encoding.

--- app.py
+++ app.py
@@ -386,7 +386,7 @@ if (selection == 'Lung Cancer Prediction'):
    
 
 if (selection == 'CNN Based disease Prediction'):
-  st.set_option('deprecation.showfileUploaderEncoding', False)
+ ## st.set_option('deprecation.showfileUploaderEncoding', False)
   @st.cache(allow_output_mutation=True)
 
   def loading_model():
